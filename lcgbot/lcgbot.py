c288ef6be10c795ff1e1b0dca78f21e6f8173468
Sjohn Chambers <sjohn.chambers@adtran.com>
2017-10-19 16:32:49 -0500

    move to attachments instead of messages

--- lcgbot/lcgbot.py
+++ lcgbot/lcgbot.py
@@ -7,6 +7,16 @@ from slackclient import SlackClient
 #In Memory Storage for now
 CARDS = None
 
+colors = {
+        'crab': "#001c94",
+        'crane': "#0089de",
+        'dragon': "#00a472",
+        'lion': "#cb9d00",
+        'phoenix': "#c16400",
+        'scorpion': "#a61600",
+        'unicorn': "#780098",
+        }
+
 def populate_cards():
     global CARDS
     r = requests.get('https://api.fiveringsdb.com/cards')
@@ -40,61 +50,119 @@ def make_key_value(key, value):
     else:
         return ''
 
-def get_field(card, field, name = None, do_title = False):
+def get_field(card, field, do_title = False):
     value = card.get(field)
     if value is None:
         return ''
     value = str(value).encode('utf-8')
     if do_title:
         value = value.title()
-    name = name if name else field
-    return make_key_value(name.title(), value)
+    return value
 
 def get_traits(card):
     traits = ", ".join(map(str,card.get('traits', []))).title().encode('utf-8')
-    return make_key_value('Traits', traits)
+    return traits
 
 def get_text(card):
     text = card.get('text')
     if text is None:
         return ''
     text = slackify_text(text.encode('utf-8'))
-    return make_key_value('Text', text)
+    return text
 
 def get_image(card):
     try:
         image = str([c['image_url'] for c in card['pack_cards'] if 'image_url' in c][0])
-        return make_key_value('Image', image)
+        return image
     except:
         return ''
 
 def get_pack(card):
     try:
         pack = str([c['pack']['id'] for c in card['pack_cards'] if 'id' in c['pack']][0])
-        return make_key_value('Pack', pack.title())
+        return pack.title()
     except:
         return ''
 
+def get_number(card):
+    try:
+        pack = str([c['position'] for c in card['pack_cards'] if 'position' in c][0])
+        return pack.title()
+    except:
+        return ''
+
+def get_color(card):
+    clan = card.get('clan')
+    return colors.get(clan, '#3c3c3c')
+
 def pprint_card(card):
     pprinted = ''
-    pprinted += get_field(card, 'name')
-    pprinted += get_pack(card)
-    pprinted += get_field(card, 'type', do_title = True)
-    pprinted += get_field(card, 'side', name = 'deck', do_title = True)
+    pprinted += make_key_value('Name', get_field(card, 'name'))
+    pprinted += make_key_value('Pack', get_field(card, 'pack'))
+    pprinted += make_key_value('Type', get_field(card, 'type', do_title = True))
+    pprinted += make_key_value('Deck', get_field(card, 'side', do_title = True))
     pprinted += '\n'
-    pprinted += get_field(card, 'cost')
-    pprinted += get_field(card, 'military')
-    pprinted += get_field(card, 'military_bonus', 'military')
-    pprinted += get_field(card, 'political')
-    pprinted += get_field(card, 'political_bonus', 'political')
-    pprinted += get_field(card, 'glory')
+    pprinted += make_key_value('Cost', get_field(card, 'cost'))
+    pprinted += make_key_value('Military', get_field(card, 'military'))
+    pprinted += make_key_value('Military', get_field(card, 'military_bonus'))
+    pprinted += make_key_value('Political', get_field(card, 'political'))
+    pprinted += make_key_value('Political', get_field(card, 'political_bonus'))
+    pprinted += make_key_value('Glory', get_field(card, 'glory'))
     pprinted += '\n'
-    pprinted += get_traits(card)
+    pprinted += make_key_value('Traits', get_traits(card))
     pprinted += '\n'
-    pprinted += get_text(card)
+    pprinted += make_key_value('Text', get_text(card))
     pprinted += '\n'
-    pprinted += get_image(card)
+    pprinted += make_key_value('Image', get_image(card))
     return pprinted.replace('\n\n\n', '\n\n')
+
+def make_title_value(card, key, name = None, value = None, short = True):
+    name = name if name else key.title()
+    value = value if value else get_field(card, key)
+    if value is not None and value != 'None' and value != '':
+        return {
+                "title": name,
+                "value": value,
+                "short": short
+                }
+    else:
+        return None
+
+
+def make_attachment(card):
+    fields = ['cost', ('military', 'military_bonus'), 'glory',
+            ('political', 'political_bonus'), 'influence']
+    attachment_fields = []
+    for field in fields:
+        if isinstance(field, tuple):
+            k,v = field
+            res = get_field(card, k)
+            if res is not None and res != 'None' and res != '':
+                attachment_fields.append(make_title_value(card, k))
+            else:
+                attachment_fields.append(make_title_value(card, v, name = k.title()))
+        else:
+            attachment_fields.append(make_title_value(card, field))
+    attachment_fields.append(make_title_value(card, 'traits', name = 'Traits', value = get_traits(card)))
+    attachment_fields.append(make_title_value(card, 'text', name = 'Text', value = get_text(card), short = False))
+    attachment_fields = [f for f in attachment_fields if f is not None]
+    return {
+            "fallback": get_field(card, 'name'),
+            "color": get_color(card),
+            "author_name": "{clan} - {side} - {card_type} [{pack} {number}]".format(
+                clan = get_field(card, 'clan', do_title = True),
+                side = get_field(card, 'side', do_title = True),
+                card_type = get_field(card, 'type', do_title = True),
+                pack = get_pack(card),
+                number = get_number(card)
+                ),
+            "title": get_field(card, 'name'),
+            "title_link": "https://fiveringsdb.com/card/{}".format(
+                get_field(card, 'name_canonical').replace(' ', '-')
+                ),
+            "fields": attachment_fields,
+            "image_url": get_image(card)
+            }
 
 if __name__ == '__main__':
     trigger = "!card"
@@ -112,12 +180,12 @@ if __name__ == '__main__':
                     card = get_matching_card(name)
                     response = 'Card not found'
                     if card is not None:
-                        response = pprint_card(card)
+                        response = make_attachment(card)
                         print response
                     sc.api_call(
                         'chat.postMessage',
                         channel=msg.get('channel'),
-                        text=response
+                        attachments=[response]
                     )
             time.sleep(1)
     else:
